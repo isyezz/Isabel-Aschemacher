6bdcd525611bfecebfeb6a127856318882fdd873
isyezz <102218200+isyezz@users.noreply.github.com>
2022-06-01 09:53:02 +0200

    Server

--- sounds.js
+++ sounds.js
@@ -80,4 +80,14 @@ slider.oninput = function() {
     output.innerHTML = this.value;
 };
 
+async function sendJSONStringWithPOST(url, jsonString) {
+    const response = await fetch(url, {
+        method: "post",
+        body: jsonString,
+    });
+}
 
+sendJSONStringWithPOST(
+    "http://localhost:3000/",
+    JSON.stringify({ test: "Dies ist ein Test" })
+);
